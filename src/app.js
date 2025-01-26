8c56cd632a0f899775892671864b92f7d56e1d95
Vikram Bhallamudi <you@example.com>
2025-01-26 23:12:55 +0530

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -1,29 +1,33 @@
 const express=require("express");
 const app=express();
 
-// app.use((req,res)=>{
+app.use((req,res,next)=>{
+next()
 // res.send("Hello from the server")
-// })
+},(req,res,next)=>{
+    // res.send("Yo")
+    next();
+})
 
-app.listen(8080,()=>{
+app.listen(8081,()=>{
     console.log("server running on port 8080")
 });
 
-app.use("/hey",(req,res)=>{
-    res.send("Hey");
-})
+// app.use("/hey",(req,res)=>{
+//     res.send("Hey");
+// })
 
-app.get("/data",(req,res)=>{
-    console.log(req.query);
-    res.send({name:"vikram"});
-})
+// app.get("/data",(req,res)=>{
+//     console.log(req.query);
+//     res.send({name:"vikram"});
+// })
 
-app.get("/data/:userInfo/:num",(req,res)=>{
-    console.log(req.params);
-    res.send({name:"vikram"});
-})
+// app.get("/data/:userInfo/:num",(req,res)=>{
+//     console.log(req.params);
+//     res.send({name:"vikram"});
+// })
 
-app.post("/data",(req,res)=>{
-    res.send("Data posted successfully");
-})
+// app.post("/data",(req,res)=>{
+//     res.send("Data posted successfully");
+// })
 
