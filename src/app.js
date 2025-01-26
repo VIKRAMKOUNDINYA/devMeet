a100ba252a387953da18092205773bc092959d00
Vikram Bhallamudi <you@example.com>
2025-01-26 22:47:22 +0530

    Basic Route Handling

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app=express();
 // res.send("Hello from the server")
 // })
 
-app.listen(8081,()=>{
+app.listen(8080,()=>{
     console.log("server running on port 8080")
 });
 
@@ -13,6 +13,17 @@ app.use("/hey",(req,res)=>{
     res.send("Hey");
 })
 
-app.use("/hello",(req,res)=>{
-    res.send("Hello");
+app.get("/data",(req,res)=>{
+    console.log(req.query);
+    res.send({name:"vikram"});
 })
+
+app.get("/data/:userInfo/:num",(req,res)=>{
+    console.log(req.params);
+    res.send({name:"vikram"});
+})
+
+app.post("/data",(req,res)=>{
+    res.send("Data posted successfully");
+})
+
