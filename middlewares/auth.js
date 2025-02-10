4bd2848bcc74504ecf3b044fb20e3c6e875f50ee
Vikram Bhallamudi <you@example.com>
2025-02-10 11:13:06 +0530

    Middlewares,schema Validations and API Validations

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,22 @@
-const adminAuth=(req,res,next)=>{
-    console.log("Admin");
-    const token="xyz"
-    const isAdmin=token==="xyz"
-    if(!isAdmin){
-        res.status(401).send("unauthorized req")
-    }
-    else{
-        next();
-    }
-}
+const jwt=require("jsonwebtoken");
+const User = require("../src/models/user");
 
-const userAuth=(req,res,next)=>{
-    console.log("User");
-    const token="abc"
-    const isUser=token==="abc"
-    if(!isUser){
-        res.status(401).send("unauthorized req")
-    }
-    else{
-        next()
+const userAuth=async (req,res,next)=>{
+    try{
+        const {token}=req.cookies;
+        if(!token){
+           throw new Error("Token is invalid!")
+        }
+        const decodedObject=jwt.verify(token,"DEVMeet@2000")
+        const {_id}=decodedObject;
+        const user=await User.findById(_id);
+        if(!user){
+            throw new Error("User not found");
+        }
+        req.user=user
+        next();
+    }catch(err){
+        res.status(400).send("Error: "+err.message);
     }
 }
-module.exports={adminAuth,userAuth}
+module.exports={userAuth}
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,66 @@
 const mongoose=require('mongoose');
+const validator=require('validator')
 const userSchema=new mongoose.Schema({
     firstName:{
         type:String
     },
-    laseName:{
+    lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        require:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address"+value)
+            }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter strong password"+value)
+            }
+        }
     },
     age:{
         type:Number
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female",'others'].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://oldweb.brur.ac.bd/wp-content/uploads/2019/03/male.jpg"
+    },
+    about:{
+        type:String,
+        default:"This is a default Description"
+    },
+    skills:{
+        type:[String],
+        validate:{
+            validator:function(value){
+                return value.length<=10;
+            },
+            message:"You cannot have more than 10 skills"
+        }
     }
+},
+{
+    timestamps:true,
 });
-
+ 
 const user=mongoose.model("User",userSchema);
 
 module.exports=user;
